0532594cebe3be0ee82a4ca910da1dadb63b4927
Miguel Montesinos <m.jimenez@zeus.local>
2024-02-14 11:40:36 +0100

    Init package

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+
+const packagename = "fallon";
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.js"),
+      name: packagename,
+      fileName: (format) => `${packagename}.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
+  plugins: [vue()],
+});
